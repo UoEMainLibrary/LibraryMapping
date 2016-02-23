210e54c5ebc796fd7c6f7ea689e2774cab1c4799
Martin Georgiev <martingeorgiev1995@gmail.com>
2016-02-23 14:03:26 +0000

    Implement new elements save

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,17 +4,31 @@ class AdminController < ApplicationController
       @elements = JSON.parse params[:elements]
 
       @elements.each do |element|
-        Element.update(element["id"],
-               :left => element["left"],
-               :top => element["top"],
-               :height => element["height"],
-               :width => element["width"],
-               :opacity => element["opacity"],
-               :angle => element["angle"],
-               :fill => element["fill"],
-               :scaleX => element["scaleX"],
-               :scaleY => element["scaleY"]
-              )
+        if Element.exists?(:id => element["id"])
+          Element.update(element["id"],
+                 :left => element["left"],
+                 :top => element["top"],
+                 :height => element["height"],
+                 :width => element["width"],
+                 :opacity => element["opacity"],
+                 :angle => element["angle"],
+                 :fill => element["fill"],
+                 :scaleX => element["scaleX"],
+                 :scaleY => element["scaleY"]
+                )
+        else
+          Element.create(
+              :left => element["left"],
+              :top => element["top"],
+              :height => element["height"],
+              :width => element["width"],
+              :opacity => element["opacity"],
+              :angle => element["angle"],
+              :fill => element["fill"],
+              :scaleX => element["scaleX"],
+              :scaleY => element["scaleY"]
+          )
+        end
 
       end
 
