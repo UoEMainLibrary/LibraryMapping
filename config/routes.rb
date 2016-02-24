a2f9cb5394b98074b0c01b24de6ed2141e72045a
Martin Georgiev <martingeorgiev1995@gmail.com>
2016-02-24 10:01:29 +0000

    Implement deleting an object from canvas #	1

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'admin#index'
 
   post '/admin' => 'admin#index'
+  delete '/admin' => 'admin#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
