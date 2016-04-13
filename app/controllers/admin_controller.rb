5951e81c0da5aa0c1ecee932e2c2bfaee004962a
Giovanni Alcantara <me@giovannialcantara.com>
2016-04-13 11:45:49 +0100

    Add validation for shelfmarks

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -23,7 +23,14 @@ class AdminController < ApplicationController
     if params[:element]
       element = JSON.parse params[:element]
 
-      save_single_element(element)
+      res = save_single_element(element)
+      p res
+      if res != true
+        respond_to do |format|
+          format.json {render :json => { :errors => res['error'] }, :status => 422}
+        end
+        return
+      end
     end
 
     head :ok
@@ -59,18 +66,18 @@ class AdminController < ApplicationController
       canvasElement = Element.new
     end
 
-      # Set element general attributes
-      canvasElement.left = element["left"]
-      canvasElement.top = element["top"]
-      canvasElement.opacity = element["opacity"]
-      canvasElement.angle = element["angle"]
-      canvasElement.fill = element["fill"]
-      canvasElement.scaleX = element["scaleX"]
-      canvasElement.scaleY = element["scaleY"]
-      canvasElement.element_type_id = element["element_type_id"]
-      canvasElement.floor = element["floor"]
+    # Set element general attributes
+    canvasElement.left = element["left"]
+    canvasElement.top = element["top"]
+    canvasElement.opacity = element["opacity"]
+    canvasElement.angle = element["angle"]
+    canvasElement.fill = element["fill"]
+    canvasElement.scaleX = element["scaleX"]
+    canvasElement.scaleY = element["scaleY"]
+    canvasElement.element_type_id = element["element_type_id"]
+    canvasElement.floor = element["floor"]
 
-      if element["element_type_id"] == ElementType.find_by(name: "Shelf").id
+    if element["element_type_id"] == ElementType.find_by(name: "Shelf").id
 
       if element["range_up_opt"] == nil
         element["range_up_opt"] = ''
@@ -97,57 +104,65 @@ class AdminController < ApplicationController
       end
 
       # Validate shelfmark
-        if (element["range_up_opt"] == "Ref. ")
-          shelfmark_up = element["range_up_letters"] + element["range_up_digits"]
-        else
-          shelfmark_up = element["range_up_opt"] + element["range_up_letters"] + element["range_up_digits"]
-        end
-
-        if (element["range_down_opt"] == "Ref. ")
-          shelfmark_down = element["range_down_letters"] + element["range_down_digits"]
-        else
-          shelfmark_down = element["range_down_opt"] + element["range_down_letters"] + element["range_down_digits"]
-        end
-
-        if shelfmark_up != ""
-          shelfmark_up = shelfmarkToOrder(shelfmark_up)
-          if shelfmark_up == -1
-            #return error
-          end
-        end
-
-        if shelfmark_down != ""
-          shelfmark_down = shelfmarkToOrder(shelfmark_down)
-          if shelfmark_down == -1
-            #return error
-          end
-        end
-
-        if shelfmark_down >= shelfmark_up
-          # return error TODO
-        end
-
-        # Update shelve's custom attribute
-        canvasElement.range_up = shelfmark_up
-        canvasElement.range_down = shelfmark_down
-        canvasElement.classification = element["classification"]
-        canvasElement.identifier = element["identifier"]
-        canvasElement.range_up_opt = element["range_up_opt"]
-        canvasElement.range_up_digits = element["range_up_digits"]
-        canvasElement.range_up_letters = element["range_up_letters"]
-        canvasElement.range_down_opt = element["range_down_opt"]
-        canvasElement.range_down_digits = element["range_down_digits"]
-        canvasElement.range_down_letters = element["range_down_letters"]
-
-      elsif element["element_type_id"] == ElementType.find_by(name: "Wall").id
-
-        canvasElement.right = element["right"]
-        canvasElement.bottom = element["bottom"]
-
+      if (element["range_up_opt"] == "Ref. ")
+        shelfmark_up = element["range_up_letters"] + element["range_up_digits"]
+      else
+        shelfmark_up = element["range_up_opt"] + element["range_up_letters"] + element["range_up_digits"]
       end
 
-      canvasElement.save
+      if (element["range_down_opt"] == "Ref. ")
+        shelfmark_down = element["range_down_letters"] + element["range_down_digits"]
+      else
+        shelfmark_down = element["range_down_opt"] + element["range_down_letters"] + element["range_down_digits"]
+      end
+
+      if shelfmark_down != ""
+        shelfmark_down = shelfmarkToOrder(shelfmark_down)
+        if shelfmark_down == -1
+          return {"error" => "Invalid start shelfmark"}
+        end
+      else
+        return {"error" => "Start shelfmark cannot be empty"}
+      end
+
+      if shelfmark_up != ""
+        shelfmark_up = shelfmarkToOrder(shelfmark_up)
+        if shelfmark_up == -1
+          return {"error" => "Invalid end shelfmark"}
+        end
+      else
+        return {"error" => "End shelfmark cannot be empty"}
+      end
+
+      if shelfmark_down > shelfmark_up
+        return {"error" => "Invalid range: start shelfmark should be lower than end shelfmark"}
+      end
+
+      # Update shelve's custom attribute
+      canvasElement.range_up = shelfmark_up
+      canvasElement.range_down = shelfmark_down
+      canvasElement.classification = element["classification"]
+      canvasElement.identifier = element["identifier"]
+      canvasElement.range_up_opt = element["range_up_opt"]
+      canvasElement.range_up_digits = element["range_up_digits"]
+      canvasElement.range_up_letters = element["range_up_letters"]
+      canvasElement.range_down_opt = element["range_down_opt"]
+      canvasElement.range_down_digits = element["range_down_digits"]
+      canvasElement.range_down_letters = element["range_down_letters"]
+
+    elsif element["element_type_id"] == ElementType.find_by(name: "Wall").id
+
+      canvasElement.right = element["right"]
+      canvasElement.bottom = element["bottom"]
 
     end
+
+    if canvasElement.save
+      return true
+    else
+      return {"error" => canvasElement.errors.full_messages}
+    end
+
+  end
 
 end
